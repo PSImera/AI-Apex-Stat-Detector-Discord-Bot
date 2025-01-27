1bcce16a8f584a4f6a305fd01e3186f43fa504e6
Bartek Zychowski <bartis1313@gmail.com>
2025-01-27 17:18:49 +0100

    fix - fix format - don't use temp files, send it by buffer

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ from stat_to_role import role_by_stats
 
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv("TOKEN")
 CONFIG_PATH = "config.json"
+
 
 def load_config():
     if os.path.exists(CONFIG_PATH):
@@ -23,9 +24,11 @@ def load_config():
             return json.load(f)
     return {}
 
+
 def save_config(config):
     with open(CONFIG_PATH, "w") as f:
         json.dump(config, f, indent=4)
+
 
 config = load_config()
 
@@ -36,17 +39,18 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
+
 @client.event
 async def on_ready():
     await tree.sync()
-    print(f'[BOT] {client.user} is ready')
+    print(f"[BOT] {client.user} is ready")
 
 
 reader = easyocr.Reader(
-    ['en', 'ru'],
-    model_storage_directory='EasyOCR_model',
-    user_network_directory='EasyOCR_user_network',
-    recog_network='apex_stats_detector'
+    ["en", "ru"],
+    model_storage_directory="EasyOCR_model",
+    user_network_directory="EasyOCR_user_network",
+    recog_network="apex_stats_detector",
 )
 
 
@@ -60,26 +64,46 @@ async def set_channel(interaction: discord.Interaction, channel: discord.TextCha
             config[guild_id] = {}
         config[guild_id]["STAT_CHANNEL_ID"] = channel.id
         save_config(config)
-        await interaction.response.send_message(f"Channel {channel.mention} has been successfully configured", ephemeral=True)
+        await interaction.response.send_message(
+            f"Channel {channel.mention} has been successfully configured",
+            ephemeral=True,
+        )
     else:
-        await interaction.response.send_message("You do not have sufficient permissions to execute this command", ephemeral=True)
+        await interaction.response.send_message(
+            "You do not have sufficient permissions to execute this command",
+            ephemeral=True,
+        )
+
 
 @tree.command(name="set_log_channel", description="Set up a channel for logs")
 @discord.app_commands.describe(channel="log channel")
-async def set_log_channel(interaction: discord.Interaction, channel: discord.TextChannel):
+async def set_log_channel(
+    interaction: discord.Interaction, channel: discord.TextChannel
+):
     if interaction.user.guild_permissions.administrator:
         guild_id = str(interaction.guild_id)
         if guild_id not in config:
             config[guild_id] = {}
         config[guild_id]["LOG_CHANNEL_ID"] = channel.id
         save_config(config)
-        await interaction.response.send_message(f"Log channel {channel.mention} has been successfully configured", ephemeral=True)
+        await interaction.response.send_message(
+            f"Log channel {channel.mention} has been successfully configured",
+            ephemeral=True,
+        )
     else:
-        await interaction.response.send_message("You do not have sufficient permissions to execute this command", ephemeral=True)
+        await interaction.response.send_message(
+            "You do not have sufficient permissions to execute this command",
+            ephemeral=True,
+        )
 
-@tree.command(name="set_skill_roles", description="skill roles. From [1] weak to [6] strong")
+
+@tree.command(
+    name="set_skill_roles", description="skill roles. From [1] weak to [6] strong"
+)
 @discord.app_commands.describe(index="Skill level", role="role on server")
-async def set_skill_roles(interaction: discord.Interaction, index: str, role: discord.Role):
+async def set_skill_roles(
+    interaction: discord.Interaction, index: str, role: discord.Role
+):
     if interaction.user.guild_permissions.administrator:
         guild_id = str(interaction.guild_id)
         if guild_id not in config:
@@ -88,13 +112,25 @@ async def set_skill_roles(interaction: discord.Interaction, index: str, role: di
             config[guild_id]["SKILL_ROLES"] = {}
         config[guild_id]["SKILL_ROLES"][index] = role.id
         save_config(config)
-        await interaction.response.send_message(f"Role {role.name} for {index} has been successfully configured", ephemeral=True)
+        await interaction.response.send_message(
+            f"Role {role.name} for {index} has been successfully configured",
+            ephemeral=True,
+        )
     else:
-        await interaction.response.send_message("You do not have sufficient permissions to execute this command", ephemeral=True)
+        await interaction.response.send_message(
+            "You do not have sufficient permissions to execute this command",
+            ephemeral=True,
+        )
 
-@tree.command(name="set_rank_roles", description="rank roles. [0] not ranked & From [1] bronze to [7] predator")
+
+@tree.command(
+    name="set_rank_roles",
+    description="rank roles. [0] not ranked & From [1] bronze to [7] predator",
+)
 @discord.app_commands.describe(index="Rank index", role="role on server")
-async def set_rank_roles(interaction: discord.Interaction, index: str, role: discord.Role):
+async def set_rank_roles(
+    interaction: discord.Interaction, index: str, role: discord.Role
+):
     if interaction.user.guild_permissions.administrator:
         guild_id = str(interaction.guild_id)
         if guild_id not in config:
@@ -103,9 +139,16 @@ async def set_rank_roles(interaction: discord.Interaction, index: str, role: dis
             config[guild_id]["RANK_ROLES"] = {}
         config[guild_id]["RANK_ROLES"][index] = role.id
         save_config(config)
-        await interaction.response.send_message(f"Role {role.name} for {index} has been successfully configured", ephemeral=True)
+        await interaction.response.send_message(
+            f"Role {role.name} for {index} has been successfully configured",
+            ephemeral=True,
+        )
     else:
-        await interaction.response.send_message("You do not have sufficient permissions to execute this command", ephemeral=True)
+        await interaction.response.send_message(
+            "You do not have sufficient permissions to execute this command",
+            ephemeral=True,
+        )
+
 
 @tree.command(name="set_mode", description="Switch between three modes")
 @discord.app_commands.describe(mode="Choose one of the three modes")
@@ -116,16 +159,23 @@ async def set_rank_roles(interaction: discord.Interaction, index: str, role: dis
         discord.app_commands.Choice(name="Both roles", value="both_roles"),
     ]
 )
-async def set_mode(interaction: discord.Interaction, mode: discord.app_commands.Choice[str]):
+async def set_mode(
+    interaction: discord.Interaction, mode: discord.app_commands.Choice[str]
+):
     if interaction.user.guild_permissions.administrator:
         guild_id = str(interaction.guild_id)
         if guild_id not in config:
             config[guild_id] = {}
         config[guild_id]["MODE"] = mode.value
         save_config(config)
-        await interaction.response.send_message(f"Bot is now in '{mode.name}' mode.", ephemeral=True)
+        await interaction.response.send_message(
+            f"Bot is now in '{mode.name}' mode.", ephemeral=True
+        )
     else:
-        await interaction.response.send_message("You do not have sufficient permissions to execute this command.", ephemeral=True)
+        await interaction.response.send_message(
+            "You do not have sufficient permissions to execute this command.",
+            ephemeral=True,
+        )
 
 
 @client.event
@@ -152,7 +202,9 @@ async def on_message(message: discord.Message):
     else:
         is_attachment = False
         await message.delete()
-        print(f'deleted spam in {message.channel.name} channel by {message.author.name}')
+        print(
+            f"deleted spam in {message.channel.name} channel by {message.author.name}"
+        )
         return
 
     if len(message.attachments) == 1:
@@ -161,11 +213,13 @@ async def on_message(message: discord.Message):
         is_only_one_attachment = False
         await message.delete()
         print(f"[error] more then 1 attachment by {message.author.name}")
-        await message.channel.send(f'{message.author.mention} [error] send only 1 attachment with your statistic screen please')
+        await message.channel.send(
+            f"{message.author.mention} [error] send only 1 attachment with your statistic screen please"
+        )
         return
-    
+
     attachment = message.attachments[0]
-    if attachment.filename.endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+    if attachment.filename.endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp")):
         is_image = True
         print(f"[IMG] {attachment.filename} by {message.author.name}")
         bytes_image = await attachment.read()
@@ -175,80 +229,137 @@ async def on_message(message: discord.Message):
         is_image = False
         await message.delete()
         print(f"[error] not image file by {message.author.name}")
-        await message.channel.send(f'{message.author.mention} [error] send only image files (png, jpg, jpeg, gif, bmp) with your statistic screen please')
+        await message.channel.send(
+            f"{message.author.mention} [error] send only image files (png, jpg, jpeg, gif, bmp) with your statistic screen please"
+        )
         return
-    
+
     if image.size[0] >= 1280 and image.size[1] >= 720:
         is_resolution_ok = True
     else:
         is_resolution_ok = False
         await message.delete()
         print(f"[error] low resolution image by {message.author.name}")
-        await message.channel.send(f'{message.author.mention} [error] send image more then 1280x720')
+        await message.channel.send(
+            f"{message.author.mention} [error] send image more then 1280x720"
+        )
         return
 
     # IF ALL OK, READ SCREEN STATS AND GIVE ROLES
-    if all([is_stat_channel, is_attachment, is_only_one_attachment, is_image, is_resolution_ok]):
+    if all(
+        [
+            is_stat_channel,
+            is_attachment,
+            is_only_one_attachment,
+            is_image,
+            is_resolution_ok,
+        ]
+    ):
+
         roles_map_skill = config[guild_id].get("SKILL_ROLES", {})
         roles_map_rank = config[guild_id].get("RANK_ROLES", {})
         log_channel = client.get_channel(config[guild_id].get("LOG_CHANNEL_ID"))
         mode = config[guild_id].get("MODE")
 
+        name_temp_file = attachment.filename
+
         # save img to temp folder and delete message
-        os.makedirs('temp', exist_ok=True)
-        file_path = f'temp/{attachment.filename}'
-        await attachment.save(file_path)
         await message.delete()
 
         stats = await stat_by_screen(image, reader, attachment.filename)
         skill, rank = await role_by_stats(**stats)
 
-        log = f'```python\n' + '\n'.join(f'{key} = {value}' for key, value in stats.items()) + '```'
+        log = (
+            f"```python\n"
+            + "\n".join(f"{key} = {value}" for key, value in stats.items())
+            + "```"
+        )
 
         # remove current roles from roles list
-        current_skill_roles = [rl_name for rl_name, rl_id in roles_map_skill.items() if discord.utils.get(message.guild.roles, id=rl_id) in message.author.roles]
-        current_rank_roles = [rl_name for rl_name, rl_id in roles_map_rank.items() if discord.utils.get(message.guild.roles, id=rl_id) in message.author.roles]
+        current_skill_roles = [
+            rl_name
+            for rl_name, rl_id in roles_map_skill.items()
+            if discord.utils.get(message.guild.roles, id=rl_id) in message.author.roles
+        ]
+        current_rank_roles = [
+            rl_name
+            for rl_name, rl_id in roles_map_rank.items()
+            if discord.utils.get(message.guild.roles, id=rl_id) in message.author.roles
+        ]
         for cr_name in current_skill_roles:
-            await message.author.remove_roles(discord.utils.get(message.guild.roles, id=roles_map_skill.get(cr_name)))
+            await message.author.remove_roles(
+                discord.utils.get(message.guild.roles, id=roles_map_skill.get(cr_name))
+            )
         for cr_name in current_rank_roles:
-            await message.author.remove_roles(discord.utils.get(message.guild.roles, id=roles_map_rank.get(cr_name)))
+            await message.author.remove_roles(
+                discord.utils.get(message.guild.roles, id=roles_map_rank.get(cr_name))
+            )
 
-        if skill == 'error':
-            await message.channel.send(f'{message.author.mention} [error] cant reed stats from this image. try make better screen')
-            await log_channel.send(log, file=discord.File(file_path))
-            print(f'[BOT] error, {message.author.name} not get role, cant read stats')
+        dc_file = discord.File(io.BytesIO(bytes_image), filename=name_temp_file)
+
+        if skill == "error":
+            await message.channel.send(
+                f"{message.author.mention} [error] cant reed stats from this image. try make better screen"
+            )
+            await log_channel.send(log, file=dc_file)
+            print(f"[BOT] error, {message.author.name} not get role, cant read stats")
             return
-        
-        if mode == 'both_roles':
-            skill_role = discord.utils.get(message.guild.roles, id=roles_map_skill.get(skill))
-            rank_role = discord.utils.get(message.guild.roles, id=roles_map_rank.get(rank))
+
+        if mode == "both_roles":
+            skill_role = discord.utils.get(
+                message.guild.roles, id=roles_map_skill.get(skill)
+            )
+            rank_role = discord.utils.get(
+                message.guild.roles, id=roles_map_rank.get(rank)
+            )
+
             await message.author.add_roles(skill_role)
             await message.author.add_roles(rank_role)
-            await message.channel.send(f'{message.author.mention} you get roles **{skill_role}** and **{rank_role}**')
-            print(f'[BOT] {message.author.name} get roles skill: {skill} & rank: {rank}')
-            await log_channel.send(f'{message.author.mention}{skill_role.mention}{rank_role.mention}'+log, file=discord.File(file_path))
-        elif mode == 'skill_only':
-            skill_role = discord.utils.get(message.guild.roles, id=roles_map_skill.get(skill))
+
+            await message.channel.send(
+                f"{message.author.mention} you get roles **{skill_role}** and **{rank_role}**"
+            )
+            print(
+                f"[BOT] {message.author.name} get roles skill: {skill} & rank: {rank}"
+            )
+            await log_channel.send(
+                f"{message.author.mention}{skill_role.mention}{rank_role.mention}"
+                + log,
+                file=dc_file,
+            )
+        elif mode == "skill_only":
+            skill_role = discord.utils.get(
+                message.guild.roles, id=roles_map_skill.get(skill)
+            )
             await message.author.add_roles(skill_role)
-            await message.channel.send(f'{message.author.mention} you get role **{skill_role}**')
-            print(f'[BOT] {message.author.name} get skill role {skill}')
-            await log_channel.send(f'{message.author.mention}{skill_role.mention}'+log, file=discord.File(file_path))
-        elif mode == 'rank_only':
-            rank_role = discord.utils.get(message.guild.roles, id=roles_map_rank.get(rank))
+
+            await message.channel.send(
+                f"{message.author.mention} you get role **{skill_role}**"
+            )
+            print(f"[BOT] {message.author.name} get skill role {skill}")
+            await log_channel.send(
+                f"{message.author.mention}{skill_role.mention}" + log,
+                file=dc_file,
+            )
+        elif mode == "rank_only":
+            rank_role = discord.utils.get(
+                message.guild.roles, id=roles_map_rank.get(rank)
+            )
             await message.author.add_roles(rank_role)
-            await message.channel.send(f'{message.author.mention} you get role **{rank_role}**')
-            print(f'[BOT] {message.author.name} get rank role {rank}')
-            await log_channel.send(f'{message.author.mention}{rank_role.mention}'+log, file=discord.File(file_path))
+
+            await message.channel.send(
+                f"{message.author.mention} you get role **{rank_role}**"
+            )
+            print(f"[BOT] {message.author.name} get rank role {rank}")
+            await log_channel.send(
+                f"{message.author.mention}{rank_role.mention}" + log,
+                file=dc_file,
+            )
         else:
             print(f"something wrong with mode setting")
 
-
-
-        os.remove(file_path)
-        shutil.rmtree('temp')
-        print(f"[BOT] temp files deleted: {file_path}")
-
     return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     client.run(TOKEN)
--- stat_to_role.py
+++ stat_to_role.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import joblib
 
+
 def season_convert(s):
     if s == 12 or s == 17:
         return -1
@@ -8,46 +9,107 @@ def season_convert(s):
         return 1
     else:
         return 0
-    
+
+
 def kd_check(kd, counted_kd, deaths):
     if abs(kd - counted_kd) < 1:
         return counted_kd
     elif not deaths:
         return kd
-    else: 
+    else:
         return counted_kd
 
+
 async def role_by_stats(**kwargs):
-    base_stats_miss = [k for k in ["kills", "deaths", "kd", "counted_kd", "avg", "is_ranked"] if k not in kwargs]
-    rank_stats_miss = [k for k in ["rank_kills", "rank_deaths", "rank_kd", "rank_counted_kd", "rank_avg", "rank_games", "season"] if k not in kwargs]
-    season_stats_miss = [k for k in ["season_kills", "season_deaths", "season_kd", "season_counted_kd", "season_avg"] if k not in kwargs]
+    base_stats_miss = [
+        k
+        for k in ["kills", "deaths", "kd", "counted_kd", "avg", "is_ranked"]
+        if k not in kwargs
+    ]
+    rank_stats_miss = [
+        k
+        for k in [
+            "rank_kills",
+            "rank_deaths",
+            "rank_kd",
+            "rank_counted_kd",
+            "rank_avg",
+            "rank_games",
+            "season",
+        ]
+        if k not in kwargs
+    ]
+    season_stats_miss = [
+        k
+        for k in [
+            "season_kills",
+            "season_deaths",
+            "season_kd",
+            "season_counted_kd",
+            "season_avg",
+        ]
+        if k not in kwargs
+    ]
 
     if base_stats_miss or (rank_stats_miss and season_stats_miss):
-        return 'error', 'error'
+        return "error", "error"
     else:
-        is_ranked = kwargs.get('is_ranked')
+        is_ranked = kwargs.get("is_ranked")
 
-        ALL_COLUMNS = ["kills", "deaths", "kd", "avg", "is_ranked", 
-                       "season", "rank_games", 
-                       "rank_kills", "rank_deaths", "rank_kd", "rank_avg",
-                       "rank_s1", "rank_s2", 
-                       "season_kills", "season_deaths", "season_kd", "season_avg", ]
+        ALL_COLUMNS = [
+            "kills",
+            "deaths",
+            "kd",
+            "avg",
+            "is_ranked",
+            "season",
+            "rank_games",
+            "rank_kills",
+            "rank_deaths",
+            "rank_kd",
+            "rank_avg",
+            "rank_s1",
+            "rank_s2",
+            "season_kills",
+            "season_deaths",
+            "season_kd",
+            "season_avg",
+        ]
 
         df = pd.DataFrame([kwargs], columns=ALL_COLUMNS)
 
-        df['kd'] = kd_check(kwargs.get('kd'), kwargs.get('counted_kd'), kwargs.get('deaths'))
+        df["kd"] = kd_check(
+            kwargs.get("kd"), kwargs.get("counted_kd"), kwargs.get("deaths")
+        )
         if is_ranked:
-            df['rank_kd'] = kd_check(kwargs.get('rank_kd'), kwargs.get('rank_counted_kd'), kwargs.get('rank_deaths'))
-            rank_map = {'unknown': 0,'bronze': 1, 'silver': 2, 'gold': 3, 'platinum': 4, 'diamond': 5, 'master': 6, 'predator': 7}
-            df['rank_s1'] = df['rank_s1'].map(rank_map).fillna(0).astype(int)
-            df['rank_s2'] = df['rank_s2'].map(rank_map).fillna(0).astype(int)
-            df['season'] = df['season'].apply(season_convert)
-            rank = str(max(df['rank_s1'][0], df['rank_s2'][0]))
+            df["rank_kd"] = kd_check(
+                kwargs.get("rank_kd"),
+                kwargs.get("rank_counted_kd"),
+                kwargs.get("rank_deaths"),
+            )
+            rank_map = {
+                "unknown": 0,
+                "bronze": 1,
+                "silver": 2,
+                "gold": 3,
+                "platinum": 4,
+                "diamond": 5,
+                "master": 6,
+                "predator": 7,
+            }
+            df["rank_s1"] = df["rank_s1"].map(rank_map).fillna(0).astype(int)
+            df["rank_s2"] = df["rank_s2"].map(rank_map).fillna(0).astype(int)
+            df["season"] = df["season"].apply(season_convert)
+            rank = str(max(df["rank_s1"][0], df["rank_s2"][0]))
         else:
-            df['season_kd'] = kd_check(kwargs.get('season_kd'), kwargs.get('season_counted_kd'), kwargs.get('season_deaths'))
-            rank = '0'
+            df["season_kd"] = kd_check(
+                kwargs.get("season_kd"),
+                kwargs.get("season_counted_kd"),
+                kwargs.get("season_deaths"),
+            )
+            rank = "0"
 
-        model = joblib.load('skill_pred_model/lgbm_model.pkl')
+        model = joblib.load("skill_pred_model/lgbm_model.pkl")
         skill = str(model.predict(df)[0])
 
         return skill, rank
